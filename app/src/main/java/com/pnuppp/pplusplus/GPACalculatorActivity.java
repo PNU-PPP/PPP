dc6b7263e8df18606e13fa9e6aebbf304f78dfca
surferwithus <dbsdud3272@naver.com>
2024-07-05 16:20:40 +0900

    대박!!

--- app/src/main/java/com/pnuppp/pplusplus/GPACalculatorActivity.java
+++ app/src/main/java/com/pnuppp/pplusplus/GPACalculatorActivity.java
@@ -8,6 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
+import kotlinx.coroutines.selects.SelectUnbiasedKt;
+
 public class GPACalculatorActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +24,27 @@ public class GPACalculatorActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private float semesterGPA(List<SubjectInfo> subjectInfo) {
+        float res = 0.0f;
+        int total_credit = 0;
+        for (SubjectInfo s : subjectInfo) {
+            total_credit += s.credit;
+            res += (s.credit * s.grade) / total_credit;
+        }
+        return res;
+    }
+
+    private float majorGPA(List<SubjectInfo> subjectInfo) {
+        float res = 0.0f;
+        int total_majorCredit = 0;
+        for (SubjectInfo s : subjectInfo) {
+            if (s.isMajor) {
+                total_majorCredit += s.credit;
+                res += (s.credit * s.grade) / total_majorCredit;
+            }
+        }
+        return res;
     }
 }
