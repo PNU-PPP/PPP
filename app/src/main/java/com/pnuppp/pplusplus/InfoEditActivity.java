db4bd53b3072d17800f65dad5afa644988d5cdc6
oxomi <osm0071@naver.com>
2024-07-10 22:39:20 +0900

    사용자에게 받은 개인정보 메인화면 프로필카드에 띄우기

--- app/src/main/java/com/pnuppp/pplusplus/InfoEditActivity.java
+++ app/src/main/java/com/pnuppp/pplusplus/InfoEditActivity.java
@@ -1,5 +1,6 @@
 package com.pnuppp.pplusplus;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
@@ -44,6 +45,14 @@ public class InfoEditActivity extends AppCompatActivity {
             editor.apply();
 
             Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
+
+            // MainActivity로 값 전달
+            Intent intent = new Intent(InfoEditActivity.this, MainActivity.class);
+            intent.putExtra("major", major);
+            intent.putExtra("student_id", studentID);
+            intent.putExtra("name", name);
+            startActivity(intent);
+
             finish();
         });
     }
