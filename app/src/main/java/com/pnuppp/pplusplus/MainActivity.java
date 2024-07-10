ca7c8b35f84e246312c2f38ef7ea58988670df8b
oxomi <osm0071@naver.com>
2024-07-10 22:39:20 +0900

    사용자에게 받은 개인정보 메인화면 프로필카드에 띄우기

--- app/src/main/java/com/pnuppp/pplusplus/MainActivity.java
+++ app/src/main/java/com/pnuppp/pplusplus/MainActivity.java
@@ -1,8 +1,10 @@
 package com.pnuppp.pplusplus;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,8 +43,30 @@ public class MainActivity extends AppCompatActivity {
             startActivity(newIntent);
         });
 
+        // 전달받은 값을 설정할 TextView
+        TextView textView8 = findViewById(R.id.textView8);
+        TextView textView9 = findViewById(R.id.textView9);
 
+        // Intent로 값 받기
+        Intent intent = getIntent();
+        String major = intent.getStringExtra("major");
+        String studentID = intent.getStringExtra("student_id");
+        String name = intent.getStringExtra("name");
 
+        if (major != null && studentID != null && name != null) {
+            textView8.setText(major);
+            textView9.setText(studentID + " " + name);
+        } else {
+            // SharedPreferences에서 값 가져오기
+            SharedPreferences sharedPref = getSharedPreferences("com.pnuppp.pplusplus.user_info", MODE_PRIVATE);
+            if(sharedPref.contains("major") && sharedPref.contains("student_id") && sharedPref.contains("name")){
+                String savedMajor = sharedPref.getString("major", "");
+                String savedStudentID = sharedPref.getString("student_id", "");
+                String savedName = sharedPref.getString("name", "");
 
+                textView8.setText(savedMajor);
+                textView9.setText(savedStudentID + " " + savedName);
+            }
+        }
     }
 }
