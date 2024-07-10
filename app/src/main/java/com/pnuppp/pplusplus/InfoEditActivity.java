689c9ff4caab3afa8e22b8523d5bcd5f254b54d0
Neibce <y@jun0.dev>
2024-07-10 20:19:33 +0900

    내 정보 입력 페이지 완성

--- app/src/main/java/com/pnuppp/pplusplus/InfoEditActivity.java
+++ app/src/main/java/com/pnuppp/pplusplus/InfoEditActivity.java
@@ -1,6 +1,11 @@
 package com.pnuppp.pplusplus;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoEditActivity extends AppCompatActivity {
@@ -8,5 +13,38 @@ public class InfoEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_edit);
+
+        EditText editTextMajor = findViewById(R.id.etMajor);
+        EditText editTextStudentID = findViewById(R.id.etStudentID);
+        EditText editTextName = findViewById(R.id.etName);
+
+        SharedPreferences sharedPref = getSharedPreferences("com.pnuppp.pplusplus.user_info", MODE_PRIVATE);
+        Button buttonSave = findViewById(R.id.button_save);
+
+        if(sharedPref.contains("major") && sharedPref.contains("student_id") && sharedPref.contains("name")){
+            editTextName.setText(sharedPref.getString("name", ""));
+            editTextStudentID.setText(sharedPref.getString("student_id", ""));
+            editTextMajor.setText(sharedPref.getString("major", ""));
+        }
+
+        buttonSave.setOnClickListener(v -> {
+            String major = editTextMajor.getText().toString();
+            String studentID = editTextStudentID.getText().toString();
+            String name = editTextName.getText().toString();
+
+            if(major.isEmpty() || studentID.isEmpty() || name.isEmpty()) {
+                Toast.makeText(this, "모든 정보를 입력해주세요", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("name", name);
+            editor.putString("student_id", studentID);
+            editor.putString("major", major);
+            editor.apply();
+
+            Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 }
