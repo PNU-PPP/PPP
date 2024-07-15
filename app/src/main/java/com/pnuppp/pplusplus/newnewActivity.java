34e579a89394a15be22912e5d6d4df02bc7a42a7
jjjjj7j <jchgeol3@gmail.com>
2024-07-15 18:00:56 +0900

    완성입니다 완성

---
+++ app/src/main/java/com/pnuppp/pplusplus/newnewActivity.java
@@ -0,0 +1,58 @@
+package com.pnuppp.pplusplus;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.kakao.vectormap.KakaoMap;
+import com.kakao.vectormap.KakaoMapReadyCallback;
+import com.kakao.vectormap.MapLifeCycleCallback;
+import com.kakao.vectormap.MapView;
+
+
+
+
+
+
+public class newnewActivity extends AppCompatActivity {
+
+    MapView mapView;
+    KakaoMap kakaoMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_newnew);
+        mapView = findViewById(R.id.map_view);
+        mapView.start(new MapLifeCycleCallback() {
+            @Override
+            public void onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출
+                Log.d("KakaoMap", "onMapDestroy: ");
+            }
+
+            @Override
+            public void onMapError(Exception error) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출
+                Log.e("KakaoMap", "onMapError: ", error);
+            }
+        }, new KakaoMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull KakaoMap map) {
+                // 정상적으로 인증이 완료되었을 때 호출
+                // KakaoMap 객체를 얻어 옵니다.
+                kakaoMap = map;
+            }
+        });
+    }
+}
