510b0b564ad5888addd04b825009c9fdfb6f5314
Neibce <y@jun0.dev>
2024-08-14 14:43:28 +0900

    오류 수정

--- app/src/main/java/com/pnuppp/pplusplus/MainActivity.java
+++ app/src/main/java/com/pnuppp/pplusplus/MainActivity.java
@@ -95,15 +95,23 @@ public class MainActivity extends AppCompatActivity {
             startActivity(myIntent);
         });
 
-        Button buttonExternalManagement = findViewById(R.id.button4);
+        Button buttonExternalManagement = findViewById(R.id.button2);
         buttonExternalManagement.setOnClickListener(v -> {
-            Intent newIntent = new Intent(MainActivity.this, ExternalManagementActivity.class);
-            startActivity(newIntent);
+                    Intent newIntent = new Intent(MainActivity.this, ExternalManagementActivity.class);
+                    startActivity(newIntent);
+        });
         
         Button noticeButton = findViewById(R.id.noticeButton);
         noticeButton.setOnClickListener(v->{
             Intent intent = new Intent(MainActivity.this, RSSDepartmentNoticeActivity.class);
             startActivity(intent);
+        });
+
+        Button extraCurricularButton = findViewById(R.id.button_extra);
+        extraCurricularButton.setOnClickListener(v->{
+            Intent intent = new Intent(MainActivity.this, ExtraCurricularActivity.class);
+            startActivity(intent);
+
         });
 
         TextView textView8 = findViewById(R.id.textView8);
