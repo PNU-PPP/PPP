55c1b77e17c9fe576f3c519f960e975409d43b6e
oxomi <osm0071@naver.com>
2024-08-14 16:02:39 +0900

    mainactivity RSS/HTML 학과 구분 로직 추가

--- app/src/main/java/com/pnuppp/pplusplus/MainActivity.java
+++ app/src/main/java/com/pnuppp/pplusplus/MainActivity.java
@@ -13,6 +13,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.util.Log;
 
 
 import java.util.Set;
@@ -83,6 +84,13 @@ public class MainActivity extends AppCompatActivity {
             showEventDialog(selectedDate, eventDescription);
         });
 
+        //버튼
+        Button buttonInfoEdit = findViewById(R.id.button2);
+        buttonInfoEdit.setOnClickListener(v -> {
+            Intent newIntent = new Intent(MainActivity.this, InfoEditActivity.class);
+            startActivity(newIntent);
+        });
+
         Button buttonnewnew = findViewById(R.id.button5);
         buttonnewnew.setOnClickListener(v -> {
             Intent newIntent = new Intent(MainActivity.this, newnewActivity.class);
@@ -95,23 +103,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(myIntent);
         });
 
-        Button buttonInfoEdit = findViewById(R.id.button2);
-        buttonInfoEdit.setOnClickListener(v -> {
-            Intent newIntent = new Intent(MainActivity.this, InfoEditActivity.class);
-            startActivity(newIntent);
-        });
-        
-        Button noticeButton = findViewById(R.id.noticeButton);
-        noticeButton.setOnClickListener(v->{
-            Intent intent = new Intent(MainActivity.this, RSSDepartmentNoticeActivity.class);
-            startActivity(intent);
-        });
 
-        Button extraCurricularButton = findViewById(R.id.button_extra);
-        extraCurricularButton.setOnClickListener(v->{
-            Intent intent = new Intent(MainActivity.this, ExtraCurricularActivity.class);
-            startActivity(intent);
-        });
+        // 공지사항 관련 버튼 설정
+        Button noticeButton = findViewById(R.id.noticeButton);
+        noticeButton.setOnClickListener(v -> openDepartmentNotice());
 
         TextView textView8 = findViewById(R.id.textView8);
         TextView textView9 = findViewById(R.id.textView9);
@@ -136,6 +131,99 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+
+    private void openDepartmentNotice() {
+        String userDepartment = getUserDepartment();
+
+        if (isRssDepartment(userDepartment)) {
+            Intent intent = new Intent(MainActivity.this, RSSDepartmentNoticeActivity.class);
+            intent.putExtra("major", userDepartment);
+            startActivity(intent);
+        } else if (isNonRssDepartment(userDepartment)) {
+            Intent intent = new Intent(MainActivity.this, HtmlDepartmentNoticeActivity.class);
+            intent.putExtra("major", userDepartment);
+            startActivity(intent);
+        } else {
+            Log.e("MainActivity", "해당 학과의 공지사항 형식을 찾을 수 없습니다.");
+        }
+    }
+
+    // 사용자의 학과 정보를 가져오는 메소드
+    private String getUserDepartment() {
+        SharedPreferences sharedPref = getSharedPreferences("com.pnuppp.pplusplus.user_info", MODE_PRIVATE);
+        return sharedPref.getString("major", ""); // 기본값은 빈 문자열
+    }
+
+    private boolean isRssDepartment(String department) {
+        for (String rssDepartment : rssDepartments) {
+            if (rssDepartment.equals(department)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNonRssDepartment(String department) {
+        for (String nonRssDepartment : nonRssDepartments) {
+            if (nonRssDepartment.equals(department)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private String[] rssDepartments = new String[]{
+            // RSS인 학과 리스트
+            "국어국문학과", "일어일문학과", "불어불문학과", "노어노문학과",
+            "중어중문학과", "영어영문학과", "독어독문학과", "언어정보학과", "사학과",
+            // 사회과학대학
+            "행정학과", "정치외교학과", "사회학과", "심리학과", "문헌정보학과", "미디어커뮤니케이션학과",
+            // 자연과학대학
+            "수학과", "통계학과", "화학과", "생명과학과", "미생물학과", "분자생물학과", "지질환경과학과",
+            "대기환경과학과", "해양학과",
+            // 공과대학
+            "고분자공학과", "유기소재시스템공학과", "전기전자공학부 전자공학전공", "전기전자공학부 전기공학전공",
+            "조선해양공학과", "재료공학부", "항공우주공학과", "건축공학과", "건축학과", "도시공학과", "사회기반시스템공학과",
+            // 사범대학
+            "국어교육과", "영어교육과", "독어교육과", "불어교육과", "교육학과", "유아교육과", "특수교육과", "일반사회교육과",
+            "역사교육과", "지리교육과", "윤리교육과", "수학교육과", "물리교육과", "화학교육과", "생물교육과", "지구과학교육과", "체육교육과",
+            // 경제통상대학
+            "무역학부", "경제학부", "관광컨벤션학과", "공공정책학부",
+            // 경영대학
+            "경영학과",
+            // 약학대학
+            "약학전공", "제약학전공",
+            // 생활과학대학
+            "아동가족학과", "의류학과", "식품영양학과",
+            // 예술대학
+            "음악학과", "한국음악학과", "미술학과", "조형학과", "디자인학과", "무용학과", "예술문화영상학과",
+            // 나노과학기술대학
+            "나노메카트로닉스공학과", "나노에너지공학과", "광메카트로닉스공학과",
+            // 생명자원과학대학
+            "식물생명과학과", "원예생명과학과", "동물생명자원과학과", "식품공학과", "생명환경화학과", "바이오소재과학과",
+            "바이오산업기계공학과", "조경학과", "식품자원경제학과", "IT응용공학과", "바이오환경에너지학과",
+            // 간호대학
+            "간호학과",
+            // 의과대학
+            "의예과", "의학과", "의과학과",
+            // 정보의생명공학대학
+            "정보컴퓨터공학부 컴퓨터공학전공", "정보컴퓨터공학부 인공지능전공", "의생명융합공학부",
+    };
+
+    private String[] nonRssDepartments = new String[]{
+            // HTML인 학과 리스트
+            "한문학과", "철학과", "고고학과", "사회복지학과", "물리학과",
+            "기계공학부", "화공생명환경공학부 화공생명공학전공",
+            "화공생명환경공학부 환경공학전공", "전기전자공학부 반도체공학전공",
+            "산업공학과", "국제학부", "실내환경디자인학과", "스포츠과학과"
+    };
+
+
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -329,4 +417,3 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 }
-
