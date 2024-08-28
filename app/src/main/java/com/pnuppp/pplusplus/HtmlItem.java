c364b1eb101a109246d11ced346b1632a31f46f8
oxomi <osm0071@naver.com>
2024-08-29 00:48:57 +0900

    RSS 학과 URL 리스트 완성 국제학부 파서 완성 디자인 카드로 고치기

--- app/src/main/java/com/pnuppp/pplusplus/HtmlItem.java
+++ app/src/main/java/com/pnuppp/pplusplus/HtmlItem.java
@@ -1,38 +1,28 @@
 package com.pnuppp.pplusplus;
 
 public class HtmlItem {
-    private String number;
     private String title;
     private String date;
-    private String attachmentCount;
-    private String views;
+    private String author; // 작성자명 필드 추가
     private String url; // URL 필드 추가
 
     // Constructor
-    public HtmlItem(String number, String title, String date, String attachmentCount, String views, String url) {
-        this.number = number;
+    public HtmlItem(String title, String date, String author, String url) {
         this.title = title;
         this.date = date;
-        this.attachmentCount = attachmentCount;
-        this.views = views;
+        this.author = author; // 추가된 필드 초기화
         this.url = url;
     }
 
     // Getters and setters
-    public String getNumber() { return number; }
-    public void setNumber(String number) { this.number = number; }
-
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
 
     public String getDate() { return date; }
     public void setDate(String date) { this.date = date; }
 
-    public String getAttachmentCount() { return attachmentCount; }
-    public void setAttachmentCount(String attachmentCount) { this.attachmentCount = attachmentCount; }
-
-    public String getViews() { return views; }
-    public void setViews(String views) { this.views = views; }
+    public String getAuthor() { return author; } // 작성자명 getter
+    public void setAuthor(String author) { this.author = author; } // 작성자명 setter
 
     public String getUrl() { return url; }
     public void setUrl(String url) { this.url = url; }
